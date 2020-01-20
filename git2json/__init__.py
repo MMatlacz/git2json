f82d0f537735b8f5a08b7d83316c72bba3217124
marcinmatlacz <marcin@matlacz.eu>
2020-01-20 15:19:41 +0100

    add origin parameter

--- git2json/__init__.py
+++ git2json/__init__.py
@@ -13,6 +13,7 @@ __author__ = 'Tavish Armstrong'
 __email__ = 'tavisharmstrong@gmail.com'
 __version__ = '0.2.3'
 
+
 # -------------------------------------------------------------------
 # Main
 
@@ -31,11 +32,17 @@ def main():
         help=('Show commits more recent than a specific date. If present, '
               'this argument is passed through to "git log" unchecked. ')
     )
+    parser.add_argument(
+        '--origin',
+        default=None,
+        help='Custom origin for getting log without making full clone of the repository'
+    )
     args = parser.parse_args()
     if sys.version_info < (3, 0):
-        print(git2json(run_git_log(args.git_dir, args.since)))
+        print(git2json(run_git_log(git_dir=args.git_dir, git_since=args.since, git_origin=args.origin)))
     else:
-        print(git2jsons(run_git_log(args.git_dir, args.since)))
+        print(git2jsons(run_git_log(git_dir=args.git_dir, git_since=args.since, git_origin=args.origin)))
+
 
 # -------------------------------------------------------------------
 # Main API functions
@@ -53,22 +60,36 @@ def git2json(fil):
 # Functions for interfacing with git
 
 
-def run_git_log(git_dir=None, git_since=None):
+def run_git_log(git_dir=None, git_since=None, git_origin=None):
     '''run_git_log([git_dir]) -> File
 
     Run `git log --numstat --pretty=raw` on the specified
     git repository and return its stdout as a pseudo-File.'''
     import subprocess
-    if git_dir:
+    if git_dir and git_origin:
         command = [
             'git',
             '--git-dir=' + git_dir,
             'log',
+            git_origin,
+            '--numstat',
+            '--pretty=raw'
+        ]
+    elif git_dir:
+        command = [
+            'git',
+            '--git-dir=' + git_dir,
+            'log',
+            git_origin,
             '--numstat',
             '--pretty=raw'
         ]
     else:
-        command = ['git', 'log', '--numstat', '--pretty=raw']
+        if git_origin:
+            command = ['git', 'log', git_origin, '--numstat', '--pretty=raw']
+        else:
+            command = ['git', 'log', '--numstat', '--pretty=raw']
+
     if git_since is not None:
         command.append('--since=' + git_since)
     raw_git_log = subprocess.Popen(
